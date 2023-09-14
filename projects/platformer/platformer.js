af632fa53d113e9c387dfe60eb6170a7edae2a2f
Bryson Nelson <bryson.nelson@sciacademy.org>
2023-09-14 16:56:33 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,8 +43,10 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
+createPlatform(0,750,2000,400);
+createPlatform((10,4800,1000,10))
+createPlatform(100,520,355,10)
+createPlatform(400,640,200,10)
 
     
     // TODO 2
@@ -52,7 +54,7 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
+createCollectable("diamond", 450, 600, 0, 0)
 
 
 
@@ -61,7 +63,7 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+createCannon("right", 660, 2000, 10, 10)
 
 
 
